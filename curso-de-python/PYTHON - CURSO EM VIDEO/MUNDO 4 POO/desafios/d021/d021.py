f6f715ef9f271cabc746b4f919a64ca8b0b6dc96
devraphaela <devraphaela@gmail.com>
2026-02-18 17:43:00 -0500

    d021

--- curso-de-python/PYTHON - CURSO EM VIDEO/MUNDO 4 POO/desafios/d021/d021.py
+++ curso-de-python/PYTHON - CURSO EM VIDEO/MUNDO 4 POO/desafios/d021/d021.py
@@ -1,1 +1,51 @@
 # DESAFIO 021 - Crie a classe Caneta, que simule o funcionamento de uma caneta colorida, podendo escrever frases na cor relativa.
+
+from rich import print
+
+class Caneta:
+    def __init__(self, cor):
+        self.cor = cor
+        self.tampada = True
+
+        if cor == 'azul':
+            self.cor_rich = 'blue'
+        elif cor == 'vermelha' or cor == 'vermelho':
+            self.cor_rich = 'red'
+        elif cor == 'verde':
+            self.cor_rich = 'green'
+        elif cor == 'amarela':
+            self.cor_rich = 'yellow'
+        elif cor == 'preta':
+            self.cor_rich = 'black'
+        else:
+            self.cor_rich = 'white'
+        
+
+    def destampar(self):
+        self.tampada = False
+    
+    
+    def escrever(self, texto): 
+        if self.tampada:
+            print('A caneta está tampada!!!')
+        else:
+            print(f'[{self.cor_rich}]{texto}[/]')
+
+
+    def quebrar_linha(self, qtd):
+        print('\n' * qtd, end='')
+
+
+
+c1 = Caneta('azul')
+c2 = Caneta('vermelha')
+c3 = Caneta('verde')
+
+c1.destampar()
+c2.destampar()
+c3.destampar()
+
+c1.escrever('Olá, tudo bem?')
+c1.quebrar_linha(2)
+c2.escrever('Olá Gafanhoto! ')
+c3.escrever('Vamos exercitar!')
