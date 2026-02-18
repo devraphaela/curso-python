548fe7124d5dc1995d4ee2131dfa619cc701be95
devraphaela <devraphaela@gmail.com>
2026-02-18 12:47:05 -0500

    update d018

--- curso-de-python/PYTHON - CURSO EM VIDEO/MUNDO 4 POO/desafios/d018/d018.py
+++ curso-de-python/PYTHON - CURSO EM VIDEO/MUNDO 4 POO/desafios/d018/d018.py
@@ -8,19 +8,19 @@ from rich.panel import Panel
 
 
 class Churrasco:
-    def __init__ (self, n, p):
-        self.nome = n
-        self.pessoas = p
+    def __init__ (self, nome, pessoas, qtde_por_pessoa=0.4, preco_carne=82.40):
+        self.nome = nome
+        self.pessoas = pessoas
+        self.qtde_por_pessoa = qtde_por_pessoa
+        self.preco_carne = preco_carne
 
     def analisar(self):
 
-        qtdeporPessoa = 0.4
-        precoCarne = 82.40
-        totalCarne = self.pessoas * qtdeporPessoa
-        totalPreco = precoCarne * totalCarne
-        precoporPessoa = totalPreco / self.pessoas
+        total_carne = self.pessoas * self.qtde_por_pessoa
+        total_preco = self.preco_carne * total_carne
+        preco_por_pessoa = total_preco / self.pessoas
 
-        texto = f"Analisando [green]{self.nome}[/] com [blue]{self.pessoas} convidados[/] \nCada participante comerá {qtdeporPessoa}Kg e cada Kg custa R${precoCarne:.2f} \nRecomendo [blue]comprar {totalCarne:.3f}Kg[/] de carne \nO custo total será de [green]R${totalPreco:.2f}[/] \nCada pessoa pagará [yellow]R${precoporPessoa}[/] para participar."
+        texto = f"Analisando [green]{self.nome}[/] com [blue]{self.pessoas} convidados[/] \nCada participante comerá {self.qtde_por_pessoa}Kg e cada Kg custa R${self.preco_carne:.2f} \nRecomendo [blue]comprar {total_carne:.3f}Kg[/] de carne \nO custo total será de [green]R${total_preco:.2f}[/] \nCada pessoa pagará [yellow]R${preco_por_pessoa:.2f}[/] para participar."
 
         painel = Panel(texto, title = f'{self.nome}')
 
